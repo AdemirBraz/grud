50378ba4fdf2ce295d16512a1ec9a752a4abf106
AdemirBraz <gordoademir2@gmail.com>
2026-05-11 20:15:49 +0900

    Trocando If Por Match e Case

--- projeto gua.py
+++ projeto gua.py
@@ -7,22 +7,23 @@ if not gordo.arquivoexiste(arq):
     gordo.criararq(arq)
 
 while True:
-    n=menu1.menu(['ver pessoas cadastradas','cadastrar nova pessoa','sair do programa'])
+    opcao=menu1.menu(['ver pessoas cadastradas','cadastrar nova pessoa','sair do programa'])
     menu1.linha()
-    if n==1:
-        gordo.lerarq(arq)
-        sleep(1)
-        continue
-    elif n==2:
-        menu1.cabeçalho('novo cadastro')
-        nome=str(input('nome: ')) 
-        idade1=menu1.leiaint('Idade: ')
-        gordo.cadastrar(arq,nome,idade1)
-        sleep(1) 
-        continue
-    elif n==3:
-        menu1.cabeçalho('seu programa acabou')
-        break   
-    else:
-        print('erro digite uma opção valida')
-    sleep(1)
+    match opcao:
+        case 1:
+            gordo.lerarq(arq)
+            sleep(1)
+            continue
+        case 2:
+            menu1.cabeçalho('novo cadastro')
+            nome=str(input('nome: ')) 
+            idade1=menu1.leiaint('Idade: ')
+            gordo.cadastrar(arq,nome,idade1)
+            sleep(1) 
+            continue
+        case 3:
+            menu1.cabeçalho('seu programa acabou')
+            break   
+        case _:
+            print('erro digite uma opção valida')
+            sleep(1)
