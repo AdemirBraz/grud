4f259d9b303a28e7053fa289adbf654a19fa7299
AdemirBraz <gordoademir2@gmail.com>
2026-05-12 10:27:36 +0900

    refeito toda a parte de excluir e erros no programa central

--- menu/menu1.py
+++ menu/menu1.py
@@ -3,11 +3,11 @@ def leiaint(msg):
         try:
             n=int(input(msg))
         except(ValueError,TypeError):
-            print('EERO::DIGITE UM NUMERO REAL VALIDO')    
+            print('ERRO::DIGITE UM NUMERO REAL VALIDO')    
         except Exception as erro:
             print(erro.__cause__)
         except KeyboardInterrupt:
-            print('O USUSARIO NAO QUIS INFORMAR O ERRO !')
+            print('O USUARIO NAO QUIS INFORMAR O ERRO !')
         else:
             return n
 def linha():
@@ -17,11 +17,9 @@ def cabeçalho(txt):
     print(f'{txt:^30}')
     linha()
 def menu(lista):
-    cabeçalho('menu de cadastro')
-    c=1
-    for items in lista:
-        print(f'{c}-{items}')
-        c+=1
+    cabeçalho('MENU DE CADASTRO')
+    for c,items in enumerate(lista, start=1):
+        print(f'{c}-{items}') 
     linha()
     opc=leiaint('SUA OPÇÃO: ')
     return opc
